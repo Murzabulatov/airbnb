132652caa542ead98769060aea31df3401ca002c
Khasan <hasan.yeah.11@gmail.com>
2020-09-09 12:04:21 +0300

    Hasan 0.1: beginning

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const dbConnect = require('./dbConnect.js');
 
 const indexRouter = require('./src/routes/index.js');
+const searchRouter = require('./src/routes/search.js');
 
 const app = express();
 dbConnect();
@@ -15,6 +16,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/search', searchRouter);
+
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) throw err;
