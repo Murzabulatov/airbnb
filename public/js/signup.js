a3fdfe38c340b1941e4328849712f7c6ce2ab3fb
Salavat <slvtz@icloud.com>
2020-09-09 17:25:53 +0300

    Authorization added

---
+++ public/js/signup.js
@@ -0,0 +1,34 @@
+const newUserForm = document.getElementById('signup');
+
+newUserForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const {
+    username: { value: username },
+    password: { value: password },
+    corpassword: { value: corpassword },
+  } = event.target;
+  console.log(username, password, corpassword);
+  if (username.length >= 4 && password.length >= 4) {
+    console.log('Ok')
+    if (password === corpassword) {
+      console.log('Ok')
+      const body = {
+        username,
+        password,
+      };
+      console.log(body);
+      const response = await fetch('/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+    } else {
+      newUserForm.insertAdjacentHTML('afterend', 'Заполните форму корректно!');
+    }
+  } else {
+    newUserForm.insertAdjacentHTML('afterend', 'Заполните форму корректно!');
+  }
+});
